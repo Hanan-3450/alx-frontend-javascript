d72a76bcd0b297b8c7a47b8d161e72133e4bf32f
Hanan <your-email@example.com>
2025-07-24 13:59:52 -0700

    Task 3: Implemented printTeacher function and interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,13 +20,24 @@ const director1: Directors = {
 
 console.log(director1);
 
-
 const teacher3: Teacher = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: false,
-    location: 'London',
-    contract: false,
+  firstName: "John",
+  lastName: "Doe",
+  fullTimeEmployee: false,
+  location: "London",
+  contract: false,
 };
 
 console.log(teacher3);
+
+// Interface for printTeacher function
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Implementation of the function
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
